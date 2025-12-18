3722174413d07207d9daf24d0975c6a85903f7a9
yuyifeng <yuyifeng@xinzhetechco.com>
2025-12-18 16:13:09 +0800

    CU-86evwwmn4 - Create user form

--- src/main/java/com/dlxz/grabitec/entity/EcPermission.java
+++ src/main/java/com/dlxz/grabitec/entity/EcPermission.java
@@ -1,4 +1,41 @@
 package com.dlxz.grabitec.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@TableName("ec_permission")
 public class EcPermission {
+
+    @TableId
+    private Long id;
+
+    @TableField("name")
+    private String permissionName;
+
+    @TableField("description")
+    private String description;
+
+    @TableField("permission_key")
+    private String permissionKey;
+
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createdAt;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private LocalDateTime updatedAt;
+
+
+
 }
