6f0939af5ad531200794c48bb30fc5ed856a43f3
yuyifeng <yuyifeng@xinzhetechco.com>
2025-12-20 15:54:42 +0800

    CU-86evwwmn4 - Create user form

--- src/main/java/com/dlxz/grabitec/SecurityConfig.java
+++ src/main/java/com/dlxz/grabitec/SecurityConfig.java
@@ -19,7 +19,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable) // 关闭 CSRF（前后端分离必须）
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
-                                "/adminlte/login", "/adminlte/register", "/**/*.js", "/**/*.css","/**/*.png","/favicon.ico")
+                                "/adminlte/login", "/adminlte/register","adminlte/index", "/**/*.js", "/**/*.css","/**/*.png","/favicon.ico")
                         .permitAll() // 注册接口允许访问
                         .anyRequest().authenticated() // 其他接口需要 session 登录
                 )
--- src/main/java/com/dlxz/grabitec/controller/adminlte/LoginController.java
+++ src/main/java/com/dlxz/grabitec/controller/adminlte/LoginController.java
@@ -1,8 +1,11 @@
 package com.dlxz.grabitec.controller.adminlte;
 
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 @RequestMapping("/adminlte")
@@ -10,6 +13,13 @@ public class LoginController {
 
     @GetMapping("/login")
     public String login() {
+        // 返回模板文件的名称（不需要加 .html 后缀）
+        // 假设你的 HTML 文件路径是 src/main/resources/templates/login.html
         return "login";
+    }
+
+    @GetMapping("/index")
+    public String index() {
+        return "index";
     }
 }
